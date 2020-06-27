95cc72279767f634db7aa5b599c1311cc89d4d2a
RichardLiao <bnmghjtyu0@gmail.com>
2020-06-27 23:36:12 +0800

    [add]: react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import TodoList from "./pages/TodoList";
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <Switch>
+        <Route exact path="/">
+          <TodoList />
+        </Route>
+      </Switch>
     </div>
   );
 }
